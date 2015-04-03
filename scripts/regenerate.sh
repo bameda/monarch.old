853547d9cc3747dc088c784d5a03b85af9aee7d4
David Barragán Merino <david.barragan@kaleidos.net>
2015-04-03 19:28:36 +0200

    Create init script

---
+++ scripts/regenerate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# For postgresql
+echo "-> Remove monarch DB"
+dropdb monarch
+echo "-> Create monarch DB"
+createdb monarch
+echo "-> Create DB tables"
+python ../manage.py migrate
+
+#echo "-> Generate sample data"
+#python ../manage.py sampledata --traceback
